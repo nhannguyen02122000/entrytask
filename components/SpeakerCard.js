941c7309d4edfc28c3d0e60501f5bb088f619191
NhanNguyenThanh <nhannguyenthanh@ShopeeVNs-MacBook-Pro.local>
2021-07-12 11:32:27 +0700

    Refactor Speaker Carx

--- components/SpeakerCard.js
+++ components/SpeakerCard.js
@@ -2,11 +2,12 @@ import Image from 'next/image'
 import React from "react";
 
 const SpeakerCard = ({style, ele }) => (
-  <div style={{...style}} className="border-2 border-gray-700 p-1">
-    <div className="relative w-full h-60">
+  <div style={{...style}} className="p-1">
+    <div className="relative w-5/6 h-5/6">
       <Image
         src={ele.picture.large}
         layout="fill"
+        alt=""
       />
     </div>
     <h2>Name: {ele.name.first}</h2>
