2fcc5851ce7572cc2b10d9e24595f203d7cc5def
NhanNguyenThanh <nhannguyenthanh@ShopeeVNs-MacBook-Pro.local>
2021-07-12 11:23:51 +0700

    Refactor Speaker Carx

--- components/SpeakerCard.js
+++ components/SpeakerCard.js
@@ -1,19 +1,17 @@
 import Image from 'next/image'
 import React from "react";
-import Card from "@material-tailwind/react/Card";
-import CardImage from "@material-tailwind/react/CardImage";
-import CardBody from "@material-tailwind/react/CardBody";
-import CardFooter from "@material-tailwind/react/CardFooter";
-import H6 from "@material-tailwind/react/Heading6";
-import Paragraph from "@material-tailwind/react/Paragraph";
-import Button from "@material-tailwind/react/Button";
 
-const SpeakerCard = ({ele, style, GUTTER_SIZE}) => {
-  return (
-    <div className="flex">
-      <img src="https://randomuser.me/api/portraits/men/91.jpg" alt=""/>
+const SpeakerCard = ({style, ele }) => (
+  <div style={{...style}} className="border-2 border-gray-700 p-1">
+    <div className="relative w-full h-60">
+      <Image
+        src={ele.picture.large}
+        layout="fill"
+      />
     </div>
-  )
-}
+    <h2>Name: {ele.name.first}</h2>
+    <h3>Phone: {ele.phone}</h3>
+  </div>
+);
 
 export default SpeakerCard;
--- pages/ssg/index.js
+++ pages/ssg/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import {FixedSizeGrid as Grid} from 'react-window'
 import AutoSizer from "react-virtualized-auto-sizer";
 import SpeakerCard from '../../components/SpeakerCard';
-import Image from 'next/image'
 
 const GUTTER_SIZE = 10;
 
@@ -15,23 +14,8 @@ export async function getStaticProps () {
   }
 }
 
-const Cell = ({style, ele }) => (
-  <div style={{...style}} className="bg-white border-2 border-gray-50">
-    <div className="relative w-full h-60">
-      <Image
-        src={ele.picture.large}
-        layout="fill"
-      />
-    </div>
-    <h2>Name: {ele.name.first}</h2>
-    <h3>Phone: {ele.phone}</h3>
-  </div>
-);
-
 const SSG = (props) => {
   const data = props.data;
-
-  console.log(data[0],"data")
   return (
     <>
       <Head>
@@ -51,7 +35,7 @@ const SSG = (props) => {
             >
               {({columnIndex, rowIndex, style}) => {
                 return (
-                  <Cell
+                  <SpeakerCard
                     ele = {data[columnIndex*3+rowIndex]}
                     style = {style}
                     GUTTER_SIZE = {GUTTER_SIZE}
